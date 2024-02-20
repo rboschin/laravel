55b08735ba9fb91ac1deeea005b4ef163b2fec94
rboschin <rboschin@users.noreply.github.com>
2024-02-20 19:10:24 +0100

    Installazione stancl

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('username')->unique();
+            $table->string('tenant_id');
             $table->rememberToken();
             $table->timestamps();
         });
